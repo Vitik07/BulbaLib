ada2d6b080b1d8a95bc11bd6a35fd3ce436abb01
Vitik07 <vita.unerbaev@gmail.com>
2025-06-23 17:42:36 +0300

    Просто скажи мне, что я твой

--- obj/Debug/net8.0/BulbaLib.AssemblyInfo.cs
+++ obj/Debug/net8.0/BulbaLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulbaLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0929858cbd42c340e493c96661408562d9787a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7c5f9027a7cef18397d82f7f0606a74201bcd75")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulbaLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulbaLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
